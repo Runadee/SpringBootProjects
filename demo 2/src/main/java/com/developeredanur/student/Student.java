4897279ea172d45e02e97f168901e688025bb634
Edanur <edanurgurgen94@gmail.com>
2024-04-05 12:16:13 +0300

    app-properties modified Student updated

--- demo 2/src/main/java/com/developeredanur/student/Student.java
+++ demo 2/src/main/java/com/developeredanur/student/Student.java
@@ -1,88 +1,26 @@
 package com.developeredanur.student;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
 import java.time.LocalDate;
 
+@Entity
+@Data
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
      private Long id;
      private String name;
      private String email;
      private LocalDate dob;
      private Integer age;
 
-    public Student() {
-    }
 
-    public Student(Long id,
-                   String name,
-                   String email,
-                   LocalDate dob,
-                   Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
-
-    public Student(String name,
-                   String email,
-                   LocalDate dob,
-                   Integer age) {
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
+    public Student(int i, String john, String doe, LocalDate of, int i1) {
     }
 }
--- demo 2/src/main/java/com/developeredanur/student/StudentService.java
+++ demo 2/src/main/java/com/developeredanur/student/StudentService.java
@@ -1,22 +1,19 @@
 package com.developeredanur.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudent(){
 
-        return List.of(
-                new Student(
-                        1L,
-                        "Eda",
-                        "eda@developer.com",
-                        LocalDate.of(2024, Month.APRIL,4),
-                        29
-                )
-        );
+     Student student = new Student(123456, "John", "Doe", LocalDate.of(2000, 5, 15), 11);
+
+       return (List<Student>) student;
     }
 
 }
